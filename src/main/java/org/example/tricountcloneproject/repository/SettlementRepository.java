3593a007045f9c41b556e412e4af7275f41e47da
hacomarch <haco_march@naver.com>
2024-03-20 21:32:49 +0900

    SettlementRepository에 있던 메서드들을 SettlementService로 로직을 분리했다. 정산 기능을 좀 더 보완했다.

--- src/main/java/org/example/tricountcloneproject/repository/SettlementRepository.java
+++ src/main/java/org/example/tricountcloneproject/repository/SettlementRepository.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 @Repository
@@ -30,173 +29,83 @@ public class SettlementRepository {
     public void save(Settlement settlement) {
         SqlParameterSource param = new BeanPropertySqlParameterSource(settlement);
         Number key = jdbcInsert.executeAndReturnKey(param);
-        settlement.setSettlement_id(key.longValue());
+        settlement.setSettlementId(key.longValue());
     }
 
-    public void delete(Long settlement_id) {
+    public void delete(Long settlementId) {
         String expenseSql = "delete from Expense where settlement_id = :id";
-        Map<String, Object> expenseParam = Map.of("id", settlement_id);
-        template.update(expenseSql, expenseParam);
+        template.update(expenseSql, Map.of("id", settlementId));
 
         String sql = "delete from Settlement where settlement_id = :id";
-        Map<String, Object> param = Map.of("id", settlement_id);
-        template.update(sql, param);
+        template.update(sql, Map.of("id", settlementId));
     }
 
-    public List<Settlement> findAll() {
-        String sql = "select * from Settlement";
-        return template.query(sql, rowMapper());
-    }
-
-    public Optional<Settlement> findById(Long settlement_id) {
+    public Optional<Settlement> findById(Long settlementId) {
         String sql = "select * from Settlement where settlement_id = :id";
         try {
-            Map<String, Object> param = Map.of("id", settlement_id);
-            Settlement settlement = template.queryForObject(sql, param, rowMapper());
-            return Optional.of(settlement);
+            Map<String, Object> param = Map.of("id", settlementId);
+            return Optional.of(template.queryForObject(sql, param, settlementRowMapper()));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
     }
 
-    //정산에 참여한 멤버 가져오기
-    public List<Member> findMembersById(Long settlement_id) {
+    public List<Settlement> findAll() {
+        String sql = "select * from Settlement";
+        return template.query(sql, settlementRowMapper());
+    }
+
+    //정산에 참여한 멤버 리스트 가져오기
+    public List<Member> findMembersById(Long settlementId) {
         String sql = "select distinct m.member_id, m.user_id, m.user_pw, m.nickname" +
                 " from Settlement s" +
                 " join Expense e on s.settlement_id = e.settlement_id" +
                 " join Member m on e.member_id = m.member_id" +
                 " where s.settlement_id = :id";
-        Map<String, Object> param = Map.of("id", settlement_id);
-        return template.query(sql, param, memberRowMapper());
+        return template.query(sql, Map.of("id", settlementId), memberRowMapper());
     }
 
-    //TODO : 정산 결과 구해서 매핑하기
-    public List<SettlementResponse> getBalance(Long settlement_id) {
-        BigDecimal balanceAmount = getBalanceAmount(settlement_id);//얼마씩 내야하는지
-        Map<Long, BigDecimal> totalAmountForSettlement = getTotalAmountForSettlement(settlement_id);
-        if (isSameAmount(totalAmountForSettlement)) {
-            return List.of();
-        }
-        List<SettlementResponse> settlementResponses = new ArrayList<>();
-
-        //멤버 각각이 얼마나 지출하거나 받아야 하는지 계산
-        for (Map.Entry<Long, BigDecimal> entry : totalAmountForSettlement.entrySet()) {
-            Long memberId = entry.getKey();
-            BigDecimal totalAmount = entry.getValue(); //멤버 1명이 총 지출한 금액
-            BigDecimal difference = balanceAmount.subtract(totalAmount); //얼마를 내야 하는지 or 얼마를 받아야 하는지
-
-            if (difference.compareTo(BigDecimal.ZERO) > 0) { //보내야 하는 경우
-                createSettlementResponses(totalAmountForSettlement, memberId, difference, settlementResponses);
-            }
-        }
-        return settlementResponses;
-    }
-
-    private boolean isSameAmount(Map<Long, BigDecimal> map) {
-        Iterator<BigDecimal> iterator = map.values().iterator();
-        BigDecimal first = iterator.next();
-        while (iterator.hasNext()) {
-            BigDecimal current = iterator.next();
-            if (!first.equals(current)) return false;
-        }
-        return true;
-    }
-
-    //응답값 만들기
-    private void createSettlementResponses(Map<Long, BigDecimal> totalAmountForSettlement,
-                                           Long memberId, BigDecimal difference,
-                                           List<SettlementResponse> settlementResponses) {
-
-        for (Map.Entry<Long, BigDecimal> receiverEntity : totalAmountForSettlement.entrySet()) {
-            Long receiverId = receiverEntity.getKey();
-            BigDecimal receiverAmount = receiverEntity.getValue();
-
-            if (!memberId.equals(receiverId) && difference.compareTo(BigDecimal.ZERO) > 0
-                    && totalAmountForSettlement.get(memberId).compareTo(BigDecimal.ZERO) != 0
-                    && receiverAmount.compareTo(BigDecimal.ZERO) != 0) {
-                totalAmountForSettlement.put(memberId, BigDecimal.ZERO);
-                totalAmountForSettlement.put(receiverId, BigDecimal.ZERO);
-
-                BigDecimal transferAmount = difference.min(receiverAmount); //보낼 금액
-                settlementResponses.add(createSettlement(memberId, transferAmount, receiverId));
-                difference = difference.subtract(transferAmount);
-            }
-        }
-    }
-
-    private SettlementResponse createSettlement(Long memberId, BigDecimal transferAmount, Long receiverId) {
-        return new SettlementResponse(memberId, findNicknameById(memberId), transferAmount.intValue(),
-                receiverId, findNicknameById(receiverId));
-    }
-
-    private String findNicknameById(Long member_id) {
-        String sql = "select nickname from Member where member_id = :id";
-        Map<String, Object> param = Map.of("id", member_id);
-        return template.queryForObject(sql, param, String.class);
-    }
-
-    //A정산에서 멤버별로 지출한 총 금액 계산
-    private Map<Long, BigDecimal> getTotalAmountForSettlement(Long settlement_id) {
+    //정산에서 멤버별로 얼마나 지출했는지 구하는 메서드
+    public Map<Long, BigDecimal> getTotalAmountByMember(Long settlementId) {
         String sql = "select member_id, sum(amount) as total_amount" +
                 " from Expense" +
                 " where settlement_id = :settlement_id" +
                 " group by member_id";
-        Map<String, Object> param = Map.of("settlement_id", settlement_id);
+        Map<String, Object> param = Map.of("settlement_id", settlementId);
+
         return template.query(sql, param, rs -> {
-            HashMap<Long, BigDecimal> totalAmounts = new HashMap<>();
+            HashMap<Long, BigDecimal> totalAmountByMember = new HashMap<>();
             while (rs.next()) {
-                Long memberId = rs.getLong("member_id");
-                BigDecimal totalAmount = rs.getBigDecimal("total_amount");
-                totalAmounts.put(memberId, totalAmount);
+                totalAmountByMember.put(
+                        rs.getLong("member_id"),
+                        rs.getBigDecimal("total_amount")
+                );
             }
-            return totalAmounts;
+            return totalAmountByMember;
         });
     }
 
-
-    //A정산에서 (총 지출 금액 / 참가자 수) 해서 각각 얼마씩 내야 하는지 구하기
-    private BigDecimal getBalanceAmount(Long settlement_id) {
-        return totalExpenseAmount(settlement_id)
-                .divide(BigDecimal.valueOf(totalMemberCount(settlement_id)), RoundingMode.UNNECESSARY)
-                .setScale(0, RoundingMode.HALF_UP);
-    }
-
-    //정산에 참여한 총 참가자수
-    private int totalMemberCount(Long settlement_id) {
+    //정산에 참여한 총 참가자 수
+    public BigDecimal totalMemberCount(Long settlementId) {
         String sql = "select count(distinct m.member_id) as member_count" +
                 " from Settlement s" +
                 " join Expense e on s.settlement_id = e.settlement_id" +
                 " join Member m on e.member_id = m.member_id" +
                 " where s.settlement_id = :id";
-        Map<String, Object> param = Map.of("id", settlement_id);
-        return template.queryForObject(sql, param, Integer.class);
+        return template.queryForObject(sql, Map.of("id", settlementId), BigDecimal.class);
     }
 
-    //정산에서 발생한 총 지출 금액
-    private BigDecimal totalExpenseAmount(Long settlement_id) {
-        List<Expense> expenses = findBySettlementId(settlement_id);
-        return expenses.stream()
-                .map(Expense::getAmount)
-                .reduce(BigDecimal.ZERO, BigDecimal::add)
-                .setScale(0, RoundingMode.HALF_UP);
+    //정산이 갖고 있는 지출 금액 리스트 가져오기
+    public List<BigDecimal> getExpensesBy(Long settlementId) {
+        String sql = "select amount from Expense where settlement_id = :id";
+        return template.queryForList(sql, Map.of("id", settlementId), BigDecimal.class);
     }
 
-    //A정산이 갖고 있는 지출 내역 가져오기
-    private List<Expense> findBySettlementId(Long settlement_id) {
-        String sql = "select * from Expense where settlement_id = :id";
-        Map<String, Object> param = Map.of("id", settlement_id);
-        return template.query(sql, param, expenseRowMapper());
-    }
-
-    public RowMapper<Settlement> rowMapper() {
+    public RowMapper<Settlement> settlementRowMapper() {
         return BeanPropertyRowMapper.newInstance(Settlement.class);
     }
 
     public RowMapper<Member> memberRowMapper() {
         return BeanPropertyRowMapper.newInstance(Member.class);
-    }
-
-    public RowMapper<Expense> expenseRowMapper() {
-        return BeanPropertyRowMapper.newInstance(Expense.class);
     }
 }
--- src/main/java/org/example/tricountcloneproject/service/SettlementService.java
+++ src/main/java/org/example/tricountcloneproject/service/SettlementService.java
@@ -4,37 +4,121 @@ import lombok.RequiredArgsConstructor;
 import org.example.tricountcloneproject.entity.Member;
 import org.example.tricountcloneproject.entity.Settlement;
 import org.example.tricountcloneproject.entity.SettlementResponse;
+import org.example.tricountcloneproject.repository.MemberRepository;
 import org.example.tricountcloneproject.repository.SettlementRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class SettlementService {
     private final SettlementRepository settlementRepository;
+    private final MemberRepository memberRepository;
 
     public void insert(Settlement settlement) {
         settlementRepository.save(settlement);
     }
 
-    public void delete(Long id) {
-        settlementRepository.delete(id);
+    public void delete(Long settlementId) {
+        settlementRepository.delete(settlementId);
     }
 
-    public Settlement findById(Long id) {
-        return settlementRepository.findById(id).get();
+    public Settlement findById(Long settlementId) {
+        return settlementRepository.findById(settlementId).get();
     }
 
-    public List<Member> findMembersById(Long id) {
-        return settlementRepository.findMembersById(id);
+    public List<Member> findMembersById(Long settlementId) {
+        return settlementRepository.findMembersById(settlementId);
     }
 
     public List<Settlement> findAll() {
         return settlementRepository.findAll();
     }
 
-    public List<SettlementResponse> getBalance(Long settlementId) {
-        return settlementRepository.getBalance(settlementId);
+    public List<SettlementResponse> getSettlementResponses(Long settlementId) {
+        BigDecimal balanceAmount = calculateBalanceAmount(settlementId); //멤버 1명이 얼마씩 내야 하는지
+        Map<Long, BigDecimal> totalExpensesByMember =
+                settlementRepository.getTotalAmountByMember(settlementId); //멤버별 정산에서 총 지출한 금액 맵 (memberId, 총 지출 금액)
+
+        if (allMembersExpenseSameAmount(totalExpensesByMember)) {
+            return Collections.emptyList();
+        }
+
+        return generateSettlementResponses(balanceAmount, totalExpensesByMember);
+    }
+
+    //정산에 참여한 참가자들이 모두 같은 금액을 지출했는지 확인하는 메서드
+    private boolean allMembersExpenseSameAmount(Map<Long, BigDecimal> map) {
+        BigDecimal first = map.values().stream().findFirst().orElse(BigDecimal.ZERO);
+        return map.values().stream().allMatch(amount -> amount.compareTo(first) == 0);
+    }
+
+    private List<SettlementResponse> generateSettlementResponses(BigDecimal balanceAmount,
+                                                                 Map<Long, BigDecimal> expensesByMember) {
+        List<SettlementResponse> responses = new ArrayList<>();
+
+        expensesByMember.forEach((memberId, totalSpent) -> {
+            if (balanceAmount.compareTo(totalSpent) > 0) { //정산 금액 > 총 지출 금액 == 더 내야 할 경우
+                adjustMemberSettlements(memberId, totalSpent, balanceAmount, expensesByMember, responses);
+            }
+        });
+
+        return responses;
+    }
+
+    private void adjustMemberSettlements(Long senderId,
+                                         BigDecimal senderTotalExpense, BigDecimal balanceAmount,
+                                         Map<Long, BigDecimal> expensesByMember,
+                                         List<SettlementResponse> responses) {
+
+        for (Map.Entry<Long, BigDecimal> entry : expensesByMember.entrySet()) {
+            // 보낼 금액이 정산 금액과 같아지면 다 보냈다는 것과 같으니 끝.
+            if (senderTotalExpense.compareTo(balanceAmount) == 0) break;
+
+            Long receiverId = entry.getKey();
+            BigDecimal receiverTotalExpense = entry.getValue();
+
+            // 지출한 금액 > 정산 금액 == 받아야 하는 멤버를 찾았으면
+            if (entry.getValue().compareTo(balanceAmount) > 0) {
+                BigDecimal amountNeededToSend = balanceAmount.subtract(senderTotalExpense);
+                BigDecimal amountNeededToReceive = balanceAmount.subtract(receiverTotalExpense).abs();
+
+                //송금자가 보내야 하는 금액과 받는 사람이 받아야 하는 금액 중 작은 금액을 결정해서 송금 금액으로 저장
+                BigDecimal sendAmount = amountNeededToSend.min(amountNeededToReceive);
+
+                responses.add(createSettlement(senderId, sendAmount, receiverId));
+                senderTotalExpense = senderTotalExpense.add(sendAmount);
+                expensesByMember.put(senderId, senderTotalExpense);
+                expensesByMember.put(receiverId, receiverTotalExpense.subtract(sendAmount));
+            }
+        }
+    }
+
+    private SettlementResponse createSettlement(Long senderId, BigDecimal sendAmount, Long receiverId) {
+        return new SettlementResponse(
+                senderId, memberRepository.findNicknameById(senderId),
+                sendAmount.intValue(),
+                receiverId, memberRepository.findNicknameById(receiverId)
+        );
+    }
+
+    //정산에서 (총 지출 금액 / 참가자 수) 해서 각각 얼마씩 내야 하는지 구하기
+    private BigDecimal calculateBalanceAmount(Long settlementId) {
+        return totalExpenseAmount(settlementId)
+                .divide(settlementRepository.totalMemberCount(settlementId), RoundingMode.HALF_UP);
+    }
+
+    //정산에서 발생한 총 지출 금액 계산하기
+    private BigDecimal totalExpenseAmount(Long settlementId) {
+        List<BigDecimal> expenses = settlementRepository.getExpensesBy(settlementId);
+        return expenses.stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .setScale(0, RoundingMode.HALF_UP);
     }
 }
